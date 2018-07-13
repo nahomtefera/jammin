bbb53cba85a6de15fb9b6bbb2acecda4a31674f5
nahom <nahumtefera@gmail.com>
2018-07-13 00:41:08 -0700

    added cancel button, added styles to every element

--- src/components/createEvent/createEvent.jsx
+++ src/components/createEvent/createEvent.jsx
@@ -9,7 +9,8 @@ class CreateEvent extends Component {
             title: "",
             location: "",
             date: "",
-            time: ""            
+            time: "",
+            description: ""           
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -29,16 +30,51 @@ class CreateEvent extends Component {
             <div className="create-event-modal-container">
                 <div onClick={()=>{this.props.toggleWindow()}} className="create-event-modal-background"></div>
                 <div className="create-event-modal-content">
-                    <button onClick={()=>{this.props.toggleWindow()}}>X</button>
-                    <fieldset>
-                        <legend>Event Info</legend>
-                        Title: <input name="title" value={this.state.title} onChange={this.handleInputChange} type="text"/> <br/>
-                        Location: <input name="location" value={this.state.location} onChange={this.handleInputChange} type="text"/> <br/>
-                        Date: <input name="date" value={this.state.date} onChange={this.handleInputChange} type="text"/> <br/>
-                        Time: <input name="time" value={this.state.time} onChange={this.handleInputChange} type="text"/> <br/>
-                    </fieldset>
 
-                    <button onClick={()=>{this.props.createEvent(this.state)}}>Create Event</button>
+                    <div className="close-button-container">
+                        <button onClick={()=>{this.props.toggleWindow()}}>
+                            <img src="https://cdn1.iconfinder.com/data/icons/random-crafticons/48/misc-_close_-2-512.png" alt=""/>
+                        </button>
+                    </div>
+
+                    <div className="create-event-modal-info-container">
+                        <div className="create-event-modal-image">
+
+                        </div>
+                        <div className="create-event-modal-info">
+                            <div className="create-event-modal-info-field">
+                                <div className="create-event-modal-info-label" htmlFor="title">Title</div>
+                                <input className="create-event-modal-info-input" name="title" value={this.state.title} onChange={this.handleInputChange} type="text"/>
+                            </div>
+                            
+                            <div className="create-event-modal-info-field">
+                                <div className="create-event-modal-info-label" htmlFor="location">Location</div>
+                                <input className="create-event-modal-info-input" name="location" value={this.state.location} onChange={this.handleInputChange} type="text"/>
+                            </div>
+                            
+                            <div className="create-event-modal-info-field">
+                                <div className="create-event-modal-info-label" htmlFor="date">Date</div>
+                                <input className="create-event-modal-info-input" name="date" value={this.state.date} onChange={this.handleInputChange} type="text"/>
+                            </div>                                
+                            
+                            <div className="create-event-modal-info-field">
+                                <div className="create-event-modal-info-label" htmlFor="time">Time</div>
+                                <input className="create-event-modal-info-input" name="time" value={this.state.time} onChange={this.handleInputChange} type="text"/>
+                            </div>
+
+                        </div>
+
+                        <div className="create-event-modal-info-field field-description">
+                                <div className="create-event-modal-info-label" htmlFor="description">Description</div><br/>
+                                <textarea className="create-event-modal-info-textarea" name="description" value={this.state.description} onChange={this.handleInputChange} type="text"> </textarea>
+                        </div>
+
+                    </div>
+                        
+                    <div className="create-cancel-buttons-container">
+                        <button className="create-event-button" onClick={()=>{this.props.createEvent(this.state)}}>Create Event</button>
+                        <button className="cancel-event-button" onClick={()=>{this.props.toggleWindow()}}>Cancel</button>
+                    </div>
                 </div>
             </div>
         )
