3443abda1cc0ade233ffe2800092cca0f2291c06
nahom <nahumtefera@gmail.com>
2018-07-13 17:44:17 -0700

    added Roboto and Lato fonts, added title to LandingPage gave styles

--- src/components/landingPage/landingPage.jsx
+++ src/components/landingPage/landingPage.jsx
@@ -7,7 +7,10 @@ class LandingPage extends Component {
         return (
             <div className="landing-page-container">
                 <div className="landing-page-intro">
-                    
+                    <h1 className="landing-page-intro-title">
+                        <span>Jammin Network</span> <br/>
+                        <span className="landing-page-intro-sub-title">Find local musicians, join a group,</span><span className="landing-page-intro-sub-title-emphasis"> Jam</span>
+                    </h1>
                 </div>
                 <div className="landing-page-info-box">
 
