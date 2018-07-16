6a2997018d9e57578b36d6dd48550ca09f1f9a10
nahom <nahumtefera@gmail.com>
2018-07-16 14:31:09 -0700

    added svg to add an non-square background, gave styles to event items

--- src/components/featuredEvents/featuredEvents.jsx
+++ src/components/featuredEvents/featuredEvents.jsx
@@ -7,7 +7,15 @@ class FeaturedEvents extends Component {
         super(props);
         
         this.state = {
-            events: [1, 2, 3, 4,]
+            events: [
+                {img:"https://goo.gl/ZUCcqq"},
+                {img:"https://goo.gl/p6bbT3"},
+                {img:"https://goo.gl/tyDAz8"},
+                {img:"https://goo.gl/8vqAcJ"},
+                {img:"https://goo.gl/Cbe2CT"},
+                {img:"https://goo.gl/aqp4dD"},
+
+            ]
         }
     }
 
@@ -18,7 +26,7 @@ class FeaturedEvents extends Component {
                 <div className="stripe">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
                     <polyline points="-2,0 100,0 100,20.3" fill="#eaeaea"></polyline>
-                        <polygon fill="white" points="0,0 100,0 100,20"/>
+                        <polygon fill="#fafafa" points="0,0 100,0 100,20"/>
                     </svg>
                 </div>
 
@@ -27,7 +35,7 @@ class FeaturedEvents extends Component {
                         return(
                                 <div key={index} className="featured-events-item">
                                     <div className="featured-events-item-img-container">
-                                        <img src="https://goo.gl/ZUCcqq" alt="band"/>
+                                        <img src={event.img} alt="band"/>
                                     </div>
 
                                     <div className="featured-events-info">
