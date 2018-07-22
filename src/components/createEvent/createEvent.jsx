5aad0b1b2397e873d9a57f49077bbc151da310b1
nahom <nahumtefera@gmail.com>
2018-07-21 18:37:52 -0700

    created function checkFields() to make sure or input fields have content and it's valid, if there is an error it will be displayed in red

--- src/components/createEvent/createEvent.jsx
+++ src/components/createEvent/createEvent.jsx
@@ -19,12 +19,23 @@ class CreateEvent extends Component {
             imageURL: false,
             title: "",
             location: "",
-            date: "",
+            date: moment().format("dddd, MMMM DD YYYY"),
             time: "",
             description: "",
             uploadedImg: false,
             startDate: moment(),
-            startTime: moment(),
+            startTime: moment("12:00","HH:mm"),
+            inputErrors: {
+                title: false,
+                title_err: "",
+                location: false,
+                location_err: "",
+                date: false,
+                time: false,
+                time_err: "",
+                description: false,
+                description_err: ""
+            }
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -32,6 +43,82 @@ class CreateEvent extends Component {
         this.createEvent = this.createEvent.bind(this);
         this.handleDateChange = this.handleDateChange.bind(this)
         this.handleTimeChange = this.handleTimeChange.bind(this)
+        this.checkFields = this.checkFields.bind(this)
+    }
+
+    checkFields(state){    
+        // This method will check if the input fields contain information
+        // And will add a message error depending on the issue
+        let prevErr = this.state.inputErrors;
+        let errors = 0;
+        // Title 
+        if(state.title === "") {
+            prevErr.title = "true";
+            prevErr.title_err = "Enter a title"
+            errors++
+        } else if(state.title.length < 8) {
+            prevErr.title = "true";
+            prevErr.title_err = "Title should be longer"
+            errors++
+        }else {
+            prevErr.title = "false";
+            prevErr.title_err = null
+        }
+        // Location
+        if(state.location === "") {
+            prevErr.location = "true";
+            prevErr.location_err = "Enter a location"    
+            errors++    
+        } else if(state.location.length < 8){
+            prevErr.location = "true";
+            prevErr.location_err = "Location should be longer"  
+            errors++          
+        } else {
+            prevErr.location = "false";
+            prevErr.location_err = null
+        }
+        // Time
+        if(state.time === "" ) {
+            prevErr.time = "true";
+            prevErr.time_err = "Select time"
+            errors++
+        } else {
+            prevErr.time = "false";
+            prevErr.time_err = null
+        }
+        // Description
+        if(state.description === "" ) {
+            prevErr.description = "true";
+            prevErr.description_err = "Enter a description";
+            errors++
+        } else {
+            prevErr.description = "false";
+            prevErr.description_err = null;
+
+        }
+
+        this.setState({inputErrors: prevErr})
+
+        return errors;
+    }
+
+    createEvent() {      
+        // We are gonna check if the input fields are correct
+        // If everything is good we will crete the event
+        if(this.checkFields(this.state) === 0) {
+            // Change ID value to the time when the event is created
+            // And then call createEvent method from parent
+            let currentTime = Date.now()
+            this.setState(
+                {id: currentTime},
+                ()=>{
+                    this.props.createEvent(this.state)
+                }
+            )
+        } else {
+            return
+        }
+        
     }
 
     handleInputChange(el) {
@@ -69,24 +156,12 @@ class CreateEvent extends Component {
         });
     }
 
-    createEvent() {
-        // Change ID value to the time when the event is created
-        // And then call createEvent method from parent
-        let currentTime = Date.now()
-        this.setState(
-            {id: currentTime},
-            ()=>{
-                this.props.createEvent(this.state)
-            }
-        )
-    }
-
     render() {
         return (
             <div className="create-event-modal-container">
                 <div onClick={()=>{this.props.toggleWindow()}} className="create-event-modal-background"></div>
                 <div className="create-event-modal-content">
-
+                    {/* CLOSE BUTTON */}
                     <div className="close-button-container">
                         <button onClick={()=>{this.props.toggleWindow()}}>
                             <img src="https://cdn1.iconfinder.com/data/icons/random-crafticons/48/misc-_close_-2-512.png" alt=""/>
@@ -94,7 +169,7 @@ class CreateEvent extends Component {
                     </div>
 
                     <div className="create-event-modal-info-container">
-
+                        {/* IMAGE */}
                         <div className="create-event-modal-image-container">
                             <div className="create-event-modal-image">
                                 {this.state.imageURL !== false ?
@@ -110,18 +185,45 @@ class CreateEvent extends Component {
                                 }
                             </label>
                         </div>
-
+                        {/* INFO INPUTS */}
                         <div className="create-event-modal-info">
+                            {/* TITLE */}
                             <div className="create-event-modal-info-field">
+                                <div className="create-event-modal-info-error">
+                                    {this.state.inputErrors.title === "true" 
+                                        ? "*" + this.state.inputErrors.title_err
+                                        : ""
+                                    }
+                                </div>
                                 <div className="create-event-modal-info-label" htmlFor="title">Title</div>
-                                <input className="create-event-modal-info-input" name="title" value={this.state.title} onChange={this.handleInputChange} type="text"/>
+                                <input 
+                                    className={this.state.inputErrors.title === "true"
+                                        ? "create-event-modal-info-input-error"
+                                        : "create-event-modal-info-input"
+                                    } 
+                                    name="title" 
+                                    value={this.state.title} 
+                                    onChange={this.handleInputChange} type="text"/>
                             </div>
-                            
+                            {/* LOCATION */}
                             <div className="create-event-modal-info-field">
+                                <div className="create-event-modal-info-error">
+                                    {this.state.inputErrors.location === "true" 
+                                        ? "*" + this.state.inputErrors.location_err
+                                        : ""
+                                    }
+                                </div>
                                 <div className="create-event-modal-info-label" htmlFor="location">Location</div>
-                                <input className="create-event-modal-info-input" name="location" value={this.state.location} onChange={this.handleInputChange} type="text"/>
+                                <input 
+                                    className={this.state.inputErrors.location === "true"
+                                        ? "create-event-modal-info-input-error"
+                                        : "create-event-modal-info-input"
+                                    } 
+                                    name="location" 
+                                    value={this.state.location} 
+                                    onChange={this.handleInputChange} type="text"/>
                             </div>
-                            
+                            {/* DATE */}
                             <div className="create-event-modal-info-field">
                                 <div className="create-event-modal-info-label" htmlFor="date">Date</div>
                                 <DatePicker
@@ -132,28 +234,51 @@ class CreateEvent extends Component {
                                     onChange={this.handleDateChange}
                                 />
                             </div>                                
-                            
+                            {/* TIME */}
                             <div className="create-event-modal-info-field">
+                                <div className="create-event-modal-info-error">
+                                    {this.state.inputErrors.time === "true" 
+                                        ? "*" + this.state.inputErrors.time_err
+                                        : ""
+                                    }
+                                </div>
                                 <div className="create-event-modal-info-label" htmlFor="time">Time</div>
                                 {/* <input className="create-event-modal-info-input" name="time" value={this.state.time} onChange={this.handleInputChange} type="text"/> */}
                                 <DatePicker
-                                    className="create-event-modal-info-input"
+                                    className={this.state.inputErrors.time === "true"
+                                        ? "create-event-modal-info-input-error"
+                                        : "create-event-modal-info-input"
+                                    }
                                     readOnly={true}
                                     selected={this.state.startTime}
                                     onChange={this.handleTimeChange}
                                     showTimeSelect
                                     showTimeSelectOnly
-                                    timeIntervals={15}
+                                    timeIntervals={30}
                                     dateFormat="LT"
                                     timeCaption="Time"
                                 />
                             </div>
 
                         </div>
-
+                        {/* DESCRIPTION */}
                         <div className="create-event-modal-info-field field-description">
+                                <div className="create-event-modal-info-error">
+                                    {this.state.inputErrors.description === "true" 
+                                        ? "*" + this.state.inputErrors.description_err
+                                        : ""
+                                    }
+                                </div>
                                 <div className="create-event-modal-info-label" htmlFor="description">Description</div><br/>
-                                <textarea className="create-event-modal-info-textarea" name="description" value={this.state.description} onChange={this.handleInputChange} type="text"> </textarea>
+                                <textarea 
+                                    className={this.state.inputErrors.description === "true"
+                                        ? "create-event-modal-info-textarea-error"
+                                        : "create-event-modal-info-textarea"
+                                    } 
+                                    name="description" 
+                                    value={this.state.description} 
+                                    onChange={this.handleInputChange} type="text"> 
+                                </textarea>
                         </div>
 
                     </div>
